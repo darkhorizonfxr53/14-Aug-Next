2a3c77cd941c2d7ed81981aa75e1d23ada8a7bc5
Amadou Sow <horizonsow@gmail.com>
2023-08-14 09:27:07 +0200

    second commit

---
+++ pages/index.js
@@ -0,0 +1,49 @@
+//Main page. This includes a header with a title and a static image
+import Header from "../components/Header"
+
+const IndexPage = () => (
+	<div className="container">
+		<Header pageTitle="Projects Page" />
+		<div className="page-content" >
+			<h1>This is the projects page!</h1>
+			<img src="/ITPic.jpg" alt="a picture" />
+      
+		 <p>Here below will be a link to my Github repository, which holds my developer projects. It contains a fully working webpage detailing myself as mentioned earlier</p>
+		 <a href='https://github.com/darkhorizonfxr53'></a>
+		 <style jsx>{`
+		      .container {
+				position: relative;
+				text-align: center;
+			  }
+		
+			  h1 {
+				position: absolute;
+				top: 40%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+				z-index: 1;
+				color: white;
+				background-color: rgba(0, 0, 0, 0.5);
+				padding: 10px;
+			  }
+		
+			  img {
+			 	display: block;
+			 	margin: 0 auto; /* Center the image horizontally */
+			 	width: 60%; /* Set the maximum width of the image */
+			 	height: auto;
+			   }
+		 `}</style>
+
+		 <style global jsx>{`
+		 	body{
+		 		background-color: #a6a69c;
+		 		font-family: "Times New Roman", serif
+		 	}
+		 	`}</style> 
+		</div>
+	</div>	
+)
+
+export default IndexPage
+
